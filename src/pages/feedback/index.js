79de85bdd9d3398866201a229db39584cb9505e7
Macintosh HD <otimusblue@gmail.com>
2019-05-26 20:33:57 +0700

    Fetch API Table

--- src/pages/feedback/index.js
+++ src/pages/feedback/index.js
@@ -6,50 +6,98 @@ import PageTitle from '../../components/PageTitle';
 import Widget from '../../components/Widget';
 import Table from '../dashboard/components/Table/Table';
 import mock from '../dashboard/mock'; 
+import { Component } from 'react';
+const columns = [
+    {
+     name: "_id",
+     label: "Name",
+     options: {
+      filter: true,
+      sort: true,
+     }
+    },
+    {
+     name: "comment",
+     label: "Company",
+     options: {
+      filter: true,
+      sort: false,
+     }
+    },
+    {
+     name: "rating",
+     label: "City",
+     options: {
+      filter: true,
+      sort: false,
+     }
+    },
+    {
+     name: "created_at",
+     label: "State",
+     options: {
+      filter: true,
+      sort: false,
+     }
+    },
+   ];   
+   const options = {
+     filterType: 'textField',
+     selectableRows: 'none'
+   };
+class Feedback extends Component{
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        isLoaded: false,
+        users: []
+      };
+    }
+  
+    componentDidMount() {
+      fetch("https://api.welcomerobot.t.s2.siouxdev.com/wr/v1/feedbacks")
+        .then(res => res.json())
+        .then(
+          (result) => {
+            this.setState({
+              isLoaded: true,
+              users: result.data
+            });
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error
+            });
+          }
+        )
+    }
+    render() {
+      const { error, isLoaded, users } = this.state;
+      if (error) {
+        return <div>Error: {error.message}</div>;
+      } else if (!isLoaded) {
+        return <div>Loading...</div>;
+      } else {
+        return (
+        //   <ul>
+        //     {users.map(user => (
+        //       <li key={user._id}>
+        //         {user.comment} {user.rating}
+        //       </li>
+        //     ))}
+        //   </ul>
+           <MUIDataTable
+         title={"Employee List"}
+        data={users}
+        columns={columns}
+        options={options}
+            />
+        );
+      }
+    }
+  }
+  
 
-const datatableData = [
- ["Joe James", "Example Inc.", "Yonkers", "NY"],
- ["John Walsh", "Example Inc.", "Hartford", "CT"],
- ["Bob Herm", "Example Inc.", "Tampa", "FL"],
- ["James Houston", "Example Inc.", "Dallas", "TX"],
- ["Prabhakar Linwood", "Example Inc.", "Hartford", "CT"],
- ["Kaui Ignace", "Example Inc.", "Yonkers", "NY"],
- ["Esperanza Susanne", "Example Inc.", "Hartford", "CT"],
- ["Christian Birgitte", "Example Inc.", "Tampa", "FL"],
- ["Meral Elias", "Example Inc.", "Hartford", "CT"],
- ["Deep Pau", "Example Inc.", "Yonkers", "NY"],
- ["Sebastiana Hani", "Example Inc.", "Dallas", "TX"],
- ["Marciano Oihana", "Example Inc.", "Yonkers", "NY"],
- ["Brigid Ankur", "Example Inc.", "Dallas", "TX"],
- ["Anna Siranush", "Example Inc.", "Yonkers", "NY"],
- ["Avram Sylva", "Example Inc.", "Hartford", "CT"],
- ["Serafima Babatunde", "Example Inc.", "Tampa", "FL"],
- ["Gaston Festus", "Example Inc.", "Tampa", "FL"],
-];
-
-const Feedbacks = props => (
-  <React.Fragment>
-    <PageTitle title="Tables" />
-    <Grid container spacing={32}>
-      <Grid item xs={12}>
-        <MUIDataTable
-          title="Employee List"
-          data={datatableData}
-          columns={["Name", "Company", "City", "State"]}
-          options={{
-            // filterType: 'id',
-            filterType:'textField',
-            searchable: false
-          }}
-        />
-      </Grid>
-      {/* <Grid item xs={12}>
-        <Widget title="Material-UI Table" upperTitle noBodyPadding>
-          <Table data={mock.table} />
-        </Widget>
-      </Grid> */}
-    </Grid>
-  </React.Fragment>
-);
-
-export default Feedbacks;
+export default Feedback;
